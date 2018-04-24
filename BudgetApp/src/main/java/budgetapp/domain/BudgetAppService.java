b5b8dffbfcf537198204762b10c1ee4e540e1a01
Niko Olsson <niko.olsson@gmail.com>
2018-04-24 21:41:00 +0300

    added a possibility to add reoccurring transactions

--- BudgetApp/src/main/java/budgetapp/domain/BudgetAppService.java
+++ BudgetApp/src/main/java/budgetapp/domain/BudgetAppService.java
@@ -2,6 +2,7 @@
 package budgetapp.domain;
 
 import budgetapp.dao.InitialDao;
+import java.time.Month;
 import java.util.List;
 
 public class BudgetAppService {
@@ -23,6 +24,17 @@ public class BudgetAppService {
             initDao.save(new Transaction(amount, month));
             return true;
         }
+    }
+    
+    public boolean addRecurringTransaction(double amount, int startingMonth, int endingMonth) {
+        if (amount == 0) {
+            return false;
+        } else {
+            for (int i = startingMonth; i <= endingMonth; i++) {
+                initDao.save(new Transaction(amount, i));
+            }
+            return true;
+        }    
     }
     
     public boolean isDouble(String input) {
@@ -64,5 +76,35 @@ public class BudgetAppService {
     
     public List<Transaction> getTransactions() {
         return initDao.findAll();
+    }
+    
+    public Enum<Month> getMonth(int value) {
+        if (value == 1) {
+            return Month.JANUARY;
+        } else if (value == 2) {
+            return Month.FEBRUARY;
+        } else if (value == 3) {
+            return Month.MARCH;
+        } else if (value == 4) {
+            return Month.APRIL;
+        } else if (value == 5) {
+            return Month.MAY;
+        } else if (value == 6) {
+            return Month.JUNE;
+        } else if (value == 7) {
+            return Month.JULY;
+        } else if (value == 8) {
+            return Month.AUGUST;
+        } else if (value == 9) {
+            return Month.SEPTEMBER;
+        } else if (value == 10) {
+            return Month.OCTOBER;
+        } else if (value == 11) {
+            return Month.NOVEMBER;
+        } else if (value == 12) {
+            return Month.DECEMBER;
+        } else {
+            return null;
+        }
     }
 }
