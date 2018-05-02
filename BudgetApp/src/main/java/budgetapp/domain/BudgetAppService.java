f8d6a0eca7bb949f0e262fe776bcc4464f67d2da
Niko Olsson <niko.olsson@gmail.com>
2018-05-02 20:01:43 +0300

    corrected errors

--- BudgetApp/src/main/java/budgetapp/domain/BudgetAppService.java
+++ BudgetApp/src/main/java/budgetapp/domain/BudgetAppService.java
@@ -103,35 +103,4 @@ public class BudgetAppService {
     public List<Transaction> getTransactions() {
         return transactionDao.findAll();
     }
-    
-//    public Enum<Month> getMonth(int value) {
-//        switch (value) {
-//            case 1:
-//                return Month.JANUARY;
-//            case 2:
-//                return Month.FEBRUARY;
-//            case 3:
-//                return Month.MARCH;
-//            case 4:
-//                return Month.APRIL;
-//            case 5:
-//                return Month.MAY;
-//            case 6:
-//                return Month.JUNE;
-//            case 7:
-//                return Month.JULY;
-//            case 8:
-//                return Month.AUGUST;
-//            case 9:
-//                return Month.SEPTEMBER;
-//            case 10:
-//                return Month.OCTOBER;
-//            case 11:
-//                return Month.NOVEMBER;
-//            case 12:
-//                return Month.DECEMBER;
-//            default:
-//                return null;
-//        }
-//    }
 }
--- BudgetApp/src/test/java/domain/BudgetAppServiceTest.java
+++ BudgetApp/src/test/java/domain/BudgetAppServiceTest.java
@@ -72,28 +72,6 @@ public class BudgetAppServiceTest {
     }
     
     @Test
-    public void getMonthGivesCorrectMonth() {
-        assertEquals(Month.JANUARY, budgetService.getMonth(1));
-        assertEquals(Month.FEBRUARY, budgetService.getMonth(2));
-        assertEquals(Month.MARCH, budgetService.getMonth(3));
-        assertEquals(Month.APRIL, budgetService.getMonth(4));
-        assertEquals(Month.MAY, budgetService.getMonth(5));
-        assertEquals(Month.JUNE, budgetService.getMonth(6));
-        assertEquals(Month.JULY, budgetService.getMonth(7));
-        assertEquals(Month.AUGUST, budgetService.getMonth(8));
-        assertEquals(Month.SEPTEMBER, budgetService.getMonth(9));
-        assertEquals(Month.OCTOBER, budgetService.getMonth(10));
-        assertEquals(Month.NOVEMBER, budgetService.getMonth(11));
-        assertEquals(Month.DECEMBER, budgetService.getMonth(12));
-    }
-    
-    @Test
-    public void getMonthDoesntReturnMonthOnIncorrectInput() {
-        assertEquals(null, budgetService.getMonth(0));
-        assertEquals(null, budgetService.getMonth(13));
-    }
-    
-    @Test
     public void addTransactionAddsToCurrentMonth() {
         assertEquals("1.0, " + localDate.getMonth() + "\n", budgetService.printAllTransactions(budgetService.getTransactionOfMonth(localDate.getMonthValue())));
     }
